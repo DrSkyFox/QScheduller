1b71e2a09516df96e2fe87c923fca2c79ee2fa49
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2021-08-05 14:13:12 +0300

    Initial commit

---
+++ qscheduller/src/main/java/com/example/qscheduller/QschedullerApplication.java
@@ -0,0 +1,13 @@
+package com.example.qscheduller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QschedullerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QschedullerApplication.class, args);
+    }
+
+}
---
+++ qscheduller/src/test/java/com/example/qscheduller/QschedullerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qscheduller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QschedullerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
